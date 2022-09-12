8a8dac3ef634eb9ed57d371667314c708c7f7835
Bingqian SHU <shu.bingqian@icloud.com>
2022-09-12 23:43:25 +0200

    fix ex3 collision calculation

--- td1/pkg/ex3sprites/sprite.go
+++ td1/pkg/ex3sprites/sprite.go
@@ -19,8 +19,8 @@ func (s* Sprite) Move(dx, dy float64){
 func Collision(s1, s2 Sprite) *Rectangle{
 	x1 := math.Max(s1.zoom * (s1.hitbox.hg.x - s1.pos.x) + s1.pos.x, s2.zoom * (s2.hitbox.hg.x - s2.pos.x) + s2.pos.x)
 	y1 := math.Max(s1.zoom * (s1.hitbox.hg.y - s1.pos.y) + s1.pos.y, s2.zoom * (s2.hitbox.hg.y - s2.pos.y) + s2.pos.y)
-	x2 := math.Min(s1.zoom * (s1.hitbox.hg.x - s1.pos.x) + s1.pos.x, s2.zoom * (s2.hitbox.hg.x - s2.pos.x) + s2.pos.x)
-	y2 := math.Min(s1.zoom * (s1.hitbox.hg.y - s1.pos.y) + s1.pos.y, s2.zoom * (s2.hitbox.hg.y - s2.pos.y) + s2.pos.y)
+	x2 := math.Min(s1.zoom * (s1.hitbox.bd.x - s1.pos.x) + s1.pos.x, s2.zoom * (s2.hitbox.bd.x - s2.pos.x) + s2.pos.x)
+	y2 := math.Min(s1.zoom * (s1.hitbox.bd.y - s1.pos.y) + s1.pos.y, s2.zoom * (s2.hitbox.bd.y - s2.pos.y) + s2.pos.y)
 	// x1 y1 = inter hg
 	// x2 y2 = inter bd
 	if x1>x2 || y1>y2 {
